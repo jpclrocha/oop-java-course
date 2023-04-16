a0fad1896bda876e42379d6f7eda7f77bdd81d7e
jpclrocha <jpclrocha@gmail.com>
2023-04-16 16:01:32 -0300

    Even numbers average problem resolved

--- section10/evenNumbersAverage.java
+++ section10/evenNumbersAverage.java
@@ -1,7 +1,35 @@
 package section10;
 
+import java.util.Scanner;
+
 public class evenNumbersAverage {
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Quantos elementos vai ter o vetor? ");
+        int qtd = sc.nextInt();
+
+        double soma = 0;
+        int pares = 0;
+        int[] arr = new int[qtd];
+
+        for (int i = 0; i < qtd; i++){
+            System.out.print("Digite um numero: ");
+            int n = sc.nextInt();
+            arr[i] = n;
+
+            if(n % 2 ==0) {
+                soma += n;
+                pares++;
+            }
+        }
+
+        if(pares != 0){
+            double media = soma / pares;
+            System.out.println("MEDIA DOS PARES = "+media);
+        }
+        else{
+            System.out.println("NENHUM NUMERO PAR");
+        }
     }
 }
