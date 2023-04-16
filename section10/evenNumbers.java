fa8ab1c38c2cf4b741b3e0ca5c8e5f661f5109e2
jpclrocha <jpclrocha@gmail.com>
2023-04-16 15:25:56 -0300

    Bigger position problem resolved

---
+++ section10/evenNumbers.java
@@ -0,0 +1,26 @@
+package section10;
+
+import java.util.Scanner;
+
+public class evenNumbers {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Quantos numeros voce vai digitar? ");
+        int num = sc.nextInt();
+
+        int[] numeros = new int[num];
+        for(int i = 0; i < num; i++){
+            System.out.print("Digite um numero: ");
+            int number = sc.nextInt();
+            numeros[i] = number;
+        }
+
+        System.out.println("Numeros pares:");
+
+        for(int i = 0; i < numeros.length; i++){
+            if(numeros[i] % 2 == 0){
+                System.out.print(numeros[i] + " ");
+            }
+        }
+    }
+}
